768d7ed86c42a27b799a46e34a2cc5c8fe487f0e
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-29 19:41:26 +0700

    Add project files.

---
+++ SK-Lib-cmath-log2-Bagian3__CPP/Source.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int x1 = 0;
+	int x2 = -73;
+
+	// Logarithmic value of 0
+	cout << "Hasil Untuk Variabel x1 (" << x1 << ") = " << log2(x1) << "\n";
+	// Logarithmic value of negative value
+	cout << "Hasil Untuk Variabel x2 (" << x2 << ") = " << log2(x2) << "\n";
+
+	_getch();
+	return 0;
+}
